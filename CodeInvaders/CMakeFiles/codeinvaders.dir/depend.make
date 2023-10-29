fe9cb214de4614eb17f9a4ad52afec585018557c
Om Rawaley <133281331+omrawaley@users.noreply.github.com>
2023-10-29 18:19:13 -0400

    Delete CodeInvaders directory

--- CodeInvaders/CMakeFiles/codeinvaders.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for codeinvaders.
-# This may be replaced when dependencies are built.
--- CodeInvaders/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o.d
+++
@@ -1,6 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Time.cpp.o: \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/src/SFML/System/Time.cpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Config.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX14.0.sdk/usr/include/TargetConditionals.h
