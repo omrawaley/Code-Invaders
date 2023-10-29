0dd66798706b537eaef8e1a9e3f4e6bfa3cabca7
Om Rawaley <133281331+omrawaley@users.noreply.github.com>
2023-10-29 18:14:34 -0400

    Delete CodeInvaders directory

--- CodeInvaders/_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o.d
+++
@@ -1,9 +0,0 @@
-_deps/sfml-build/src/SFML/Graphics/CMakeFiles/sfml-graphics.dir/Vertex.cpp.o: \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/src/SFML/Graphics/Vertex.cpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Graphics/Vertex.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Graphics/Export.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Config.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX14.0.sdk/usr/include/TargetConditionals.h \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Graphics/Color.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Vector2.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Vector2.inl
--- CodeInvaders/_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o.d
+++
@@ -1,8 +0,0 @@
-_deps/sfml-build/src/SFML/System/CMakeFiles/sfml-system.dir/Clock.cpp.o: \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/src/SFML/System/Clock.cpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Clock.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Export.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/Config.hpp \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX14.0.sdk/usr/include/TargetConditionals.h \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/include/SFML/System/Time.hpp \
-  /Users/om/C++/CodeInvaders/_deps/sfml-src/src/SFML/System/Unix/ClockImpl.hpp
